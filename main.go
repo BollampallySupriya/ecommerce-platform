1b17039fb0769c292f98e076d73e2b2ea8a27433
supriya <bollampallysupriya@gmail.com>
2024-10-26 18:05:36 +0530

    added air package for auto detecting changes

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	// "context"
-	// "fmt"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
 	// "os"
 	// "os/signal"
+	"github.com/go-chi/chi/v5"
 )
+
+type RouteResponse struct {
+	Message string `json:"message"`
+}
 
 func main() {
 
+	fmt.Println("Hello Riya!!!")
 
+	router := *chi.NewRouter()
+
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-type", "application/json")
+		json.NewEncoder(w).Encode(RouteResponse{Message: "Hello Riya!"})
+	})
+	log.Fatal(http.ListenAndServe(":5000", &router))
 }
+
